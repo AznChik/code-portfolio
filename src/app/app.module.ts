c1f9ff9735daab0f3ee24a7be66685459eac5490
Maslanka <olivia.y.maslanka@accenture.com>
2023-09-22 13:25:29 -0400

    Implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ForecastComponent } from './weather/components/forecast/forecast.component';
-import { TempWeatherComponent } from './weather/components/forecast/temp-weather/temp-weather.component';
-import { CountriesComponent } from './weather/components/form/countries/countries.component';
-import { FormComponent } from './weather/components/form/form.component';
-import { StatesComponent } from './weather/components/form/states/states.component';
-import { InfoModalComponent } from './weather/components/info-modal/info-modal.component';
-import { SectionForecastComponent } from './weather/components/info-modal/section-forecast/section-forecast.component';
-import { SectionFormComponent } from './weather/components/info-modal/section-form/section-form.component';
-import { SectionInfoComponent } from './weather/components/info-modal/section-info/section-info.component';
-import { SectionSeasonComponent } from './weather/components/info-modal/section-season/section-season.component';
-import { WeatherComponent } from './weather/weather.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ForecastComponent } from './pages/weather/components/forecast/forecast.component';
+import { TempWeatherComponent } from './pages/weather/components/forecast/temp-weather/temp-weather.component';
+import { CountriesComponent } from './pages/weather/components/form/countries/countries.component';
+import { FormComponent } from './pages/weather/components/form/form.component';
+import { StatesComponent } from './pages/weather/components/form/states/states.component';
+import { InfoModalComponent } from './pages/weather/components/info-modal/info-modal.component';
+import { SectionForecastComponent } from './pages/weather/components/info-modal/section-forecast/section-forecast.component';
+import { SectionFormComponent } from './pages/weather/components/info-modal/section-form/section-form.component';
+import { SectionInfoComponent } from './pages/weather/components/info-modal/section-info/section-info.component';
+import { SectionSeasonComponent } from './pages/weather/components/info-modal/section-season/section-season.component';
+import { WeatherComponent } from './pages/weather/weather.component';
 
 @NgModule({
   bootstrap: [ AppComponent ],
@@ -22,6 +24,7 @@ import { WeatherComponent } from './weather/weather.component';
     CountriesComponent,
     ForecastComponent,
     FormComponent,
+    HomeComponent,
     InfoModalComponent,
     SectionForecastComponent,
     SectionFormComponent,
@@ -32,6 +35,7 @@ import { WeatherComponent } from './weather/weather.component';
     WeatherComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
